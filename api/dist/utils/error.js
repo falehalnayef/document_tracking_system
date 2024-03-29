41baa4666ee78be71dcda08ccc9f9f39185020ec
falehalnayef <alnayef.faleh@gmail.com>
2024-03-29 22:40:39 +0300

    remove the dist folder

--- api/dist/utils/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class StatusError extends Error {
-    constructor(statusCode, message) {
-        super(message);
-        this.statusCode = statusCode;
-    }
-}
-exports.default = StatusError;
