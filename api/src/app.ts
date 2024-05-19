cbf435cabd63ee3248417519ec33f11cd1e93d15
falehalnayef <alnayef.faleh@gmail.com>
2024-05-19 15:04:41 +0300

    add cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,10 +3,17 @@ import indexRouter from "./routes/index.router.js";
 import Scheduler from "./services/utility_services/scheduler.service.js";
 import db from "./data/database/db.js";
 import morgan from "morgan";
+import cors from "cors";
 import errorHandlerMiddlware from "./middlewares/handlers/error.handler.handler.js";
 const app: Application = express();
 const scheduler = new Scheduler();
 
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json()); // So express can handle Requests that include JSON in the body.
 //app.use(express.urlencoded());
